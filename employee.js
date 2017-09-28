4562aef398258432ed3301f6b7a19381f704f355
albertagung <albiardtya@gmail.com>
2017-09-28 23:27:39 +0700

    Semoga betul, release 1 minus remove

--- employee.js
+++ employee.js
@@ -1,3 +1,8 @@
+let fs = require('fs')
+let employeeDbJson = fs.readFileSync('employeeDb.json','utf8')
+let employeeDb = JSON.parse(employeeDbJson);
+// let Hospital = require('./hospital')
+
 class Employee {
   constructor(name, position, username, password) {
     this.name = name
@@ -5,6 +10,31 @@ class Employee {
     this.username = username
     this.password = password
   }
+  static listEmployees(){
+    let employeeArrList = [];
+    for(var i = 0; i < employeeDb.length; i++){
+      employeeArrList.push(`${i+1}. Employee Name: ${employeeDb[i].employee_name}, Employee Position: ${employeeDb[i].employee_position}`);
+    }
+    return employeeArrList.join('\n')
+  }
+  static viewEmployeeRecord(){
+    let employeeRecArr = [];
+    for(var i = 0; i < employeeDb.length; i++){
+      employeeRecArr.push(employeeDb[i]);
+    }
+    return employeeRecArr
+  }
+  static employeeAuth(){
+    let employeeArr = [];
+    for(var i = 0; i < employeeDb.length; i++){
+      // console.log(employeeDb[i]);
+      employeeArr.push(employeeDb[i]);
+    }
+    // console.log(employeeArr[0])
+    return employeeArr
+  }
 }
 
+// let employee = new Employee();
+// employee.employeeAuth();
 module.exports = Employee
