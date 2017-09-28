feae39e4d15a7b7c10dd994b73ede9dfd1c5ec02
chandrabuwana <cbuwana@gmail.com>
2017-09-28 23:05:45 +0700

    hospital done

--- patient.js
+++ patient.js
@@ -1,3 +1,4 @@
+
 class Patient {
   constructor(id, name, diagnosis) {
     this.id = id
@@ -5,5 +6,7 @@ class Patient {
     this.diagnosis = diagnosis
   }
 }
-
-module.exports = Patient
+let patient1 = new Patient(1, 'Budi', 'Jantung')
+let patient2 = new Patient(2, 'Badu', 'Stroke')
+let patient3 = new Patient(3, 'Tono', 'Tipes')
+module.exports = Patient,patient1,patient2,patient3
